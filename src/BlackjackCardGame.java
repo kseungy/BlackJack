e760c16dac74c7540832be8a754bcc12c9516ced
jk071 <jk071@DESKTOP-OCF8F5L>
2021-12-06 20:57:33 +0900

    This is CSE2016 Project

--- src/BlackjackCardGame.java
+++ src/BlackjackCardGame.java
@@ -1,5 +1,11 @@
 
 public class BlackjackCardGame {
 
-	
+	public static void main(String[] args) {
+		
+		Dealer dealer = new Dealer();
+		new BlackjackController(dealer).manageBlackjack();
+
+	}
+
 }
