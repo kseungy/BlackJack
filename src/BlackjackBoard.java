626211ba21d3b2b3f09f735e7dfe49da6eeebf24
kseungy <95521834+kseungy@users.noreply.github.com>
2021-12-16 12:56:21 +0900

    Update BlackjackBoard.java

--- src/BlackjackBoard.java
+++ src/BlackjackBoard.java
@@ -2,27 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BlackjackBoard extends JPanel{
-	private Card card;
-	private CardDeck computer_deck;
-	private CardDeck player_deck;
-	private ComputerPlayer npc;
-	private HumanPlayer player;
-	
+	private Image background=new ImageIcon(BlackjackBoard.class.getResource("/image/gameboard.jpg")).getImage();
+
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.green);
 		g.fillRect(0, 0, 1000, 600);
 		
-		//computer 카드 자리
-		g.setColor(Color.black);
-		g.drawRect(450, 30, 100, 150);
-		g.drawRect(600, 30, 100, 150);
+		//gameboard 이미지 삽입
+		g.drawImage(background, 0, 0, null);
 		
-		//player 카드 자리
-		g.drawRect(450, 400, 100, 150);
-		g.drawRect(600, 400, 100, 150);
-		
-		//상황판(승자, 버스트, 블랙잭 출력판)
-		g.drawRect(400, 250, 350, 80);
-		
-	}	
+
+	}
+	
+	
+	
 }
