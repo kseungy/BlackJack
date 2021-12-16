55c8f87cc658d80e9a8d6b48ff0c32490bb7fa35
kseungy <95521834+kseungy@users.noreply.github.com>
2021-12-16 12:59:26 +0900

    Update HumanPlayer.java

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,20 +1,17 @@
-
 import javax.swing.*;
 
 public class HumanPlayer extends CardPlayer {
 	
 	private String name;	
-	private int chips = 0;
-
+	private double chips = 0;
+	private Card card;
+	private CardDeck deck;
+	
 	public HumanPlayer(int max_cards, String n) {
 		super(max_cards);
 		name = n;
 	}
 	
-	public boolean wantsACard() {
-		String response = JOptionPane.showInputDialog("한장 더 드릴까요? (Y/N)");
-		return response.equals("Y") || response.equals("y");
-	}
 	
 	public void youWin() {
 		chips += 1;
@@ -31,8 +28,14 @@ public class HumanPlayer extends CardPlayer {
 	public void youDraw() {
 		
 	}
+	public void youSurrender() {
+		chips -=0.5;
+	}
 	
-	public int countChips() {
+	public double countChips() {
 		return chips;
 	}
+	
+	
+
 }
